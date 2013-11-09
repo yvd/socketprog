7e32af52d85ef7922576aed37fbd37ad50ade0c0
YEDDU VAMSIDHAR <vamsii@nsl-134.cse.iitb.ac.in>
2013-11-09 22:59:51 +0530

    dest node

--- node.cpp
+++ node.cpp
@@ -11,6 +11,12 @@
 
 std::vector<std::string> addr,port,finfo;//addr stores the addresses,port stores the port numbers and finfo stores the file info
 int n;// number of nodes
+
+int md5modn(int a, int b)
+{
+	return 1;//lol
+}
+
 void loadcfg()// loads FileMesh.cfg i.e sets n, addr and finfo according to the file.
 {
 	std::string f;
@@ -45,14 +51,14 @@ int main(int argc, char const *argv[])
 {
 	if(argc != 2)
 	{
-		std::cout<<"Give the node number.\nExiting.......\n";
+		std::cout<<"You forgot to give the node number :P\nExiting.......\n";
 		return -1;
 	}
 	int node_id = atoi(argv[1]),c;
-	char buffer[10000];
+	char recieve_buffer[1000];
 	loadcfg();
 	int sockfd;
-	struct sockaddr_in my_addr, client;
+	struct sockaddr_in my_addr, client, dest_node;
 	socklen_t clientlength = sizeof(client);
 	sockfd = socket(PF_INET,SOCK_DGRAM,0);
 	my_addr.sin_family = AF_INET;
@@ -61,19 +67,35 @@ int main(int argc, char const *argv[])
 	memset(&(my_addr.sin_zero),'\0',8);//zero the rest of the struct
 	bind(sockfd,(struct sockaddr *)&my_addr, sizeof(struct sockaddr));
 	
-	do{
-		int k = recvfrom(sockfd,buffer,10000,0,(struct sockaddr*)&client,&clientlength);
+	do{  //k gives the number of bytes recieved
+		int k = recvfrom(sockfd,recieve_buffer,1000,0,(struct sockaddr*)&client,&clientlength);
 		if(k < 0)
 		{
-			cout<<"Error in Receiving data.\nExiting...........\n";
-			return -1;
+			std::cout<<"Error in Receiving data :(.\nContinuing to listen\n";
+			continue;
 		}
 		else if(k > 0)
-		{
-			int md5sum = ;//md5sum
-			int z = md5sum % n;
-			if(z == node_id)
+		{	
+			std::string pkt = std::string(recieve_buffer);//converting the msg to string format
+			int md5sum = 0;//md5sum
+			int z = md5modn(md5sum,n);
+			if(z != node_id)//current node is not the destination node for the client
 			{
+				std::string dst_addr = addr[z],dst_finfo = finfo[z];//get the info of the destination node
+				int dst_port = atoi(port[z].c_str());
+				//To send data to that node, create a struct sockaddr for that node
+                dest_node.sin_family = AF_INET; //assigning family set
+                dest_node.sin_port = htons(dst_port); //assigning a port number
+                dest_node.sin_addr.s_addr = inet_addr(dst_addr.c_str()); //assigning ip address
+                memset(&(dest_node.sin_zero),'\0',8); //the rest of the struct is given value zero
+                //Recieved data is now transferred to the destination node.
+                //k1 gives the number of bytes sent
+                int k1 = sendto(sockfd,recieve_buffer,1000,0,(struct sockaddr*)&dest_node,sizeof(struct sockaddr)); 
+                if(k1<0) //Sending failed as number of bytes sent is less than 0  :/
+                {
+                    std::cout<<"Sending to the Destination Node "<<z<<" failed :/\nExiting.......\n";  
+                    return 0;
+                }
 			}
 			else
 			{
